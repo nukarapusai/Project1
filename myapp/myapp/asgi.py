a231e968fbf47a4e70785cc878afeeb963a559aa
sai <nukarapusai@gmail.com>
2026-03-10 22:32:01 +0530

    test code

--- myapp/myapp/asgi.py
+++ myapp/myapp/asgi.py
@@ -3,7 +3,7 @@ ASGI config for myapp project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
-For more information on this file, see
+For more information on this file, see the
 https://docs.djangoproject.com/en/6.0/howto/deployment/asgi/
 """
 
